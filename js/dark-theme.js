80d1bd7343c7a856c38058c11ac9d2e4255beaf6
jean <jean.beltrame@lecom.com.br>
2023-06-07 12:53:27 -0300

    Início da lógica para alteração de tema

--- js/dark-theme.js
+++ js/dark-theme.js
@@ -1,29 +1,61 @@
+document.addEventListener("onLoad",init);
+
+function init() {
+    // document.getElementById("btn-toggle-theme").addEventListener("click",toggleTheme);
+    verifyDarkTheme(isDarkTheme());
+}
+
+function isDarkTheme() {
+    var isDarkTheme = Cookies.get('dark-theme');
+    return isDarkTheme ? isDarkTheme: false;
+}
+
+function verifyDarkTheme(isDarkTheme = false) { // toggleTheme
+    Cookies.set('dark-theme', isDarkTheme);
+    // ----------------- lógica de alteração do tema
+    if(isDarkTheme){
+        applyDarkTheme();
+    } else {
+
+    }
+    // ----------------- lógica de alteração do tema
+}
+
+function applyDarkTheme() {
+    changeMenuToDark();
+    changeBodyToDark();
+}
+
 function darkTheme(){
     changeBody();
     changeMenu();
     changeButtons()
 }
 
-function changeBody(){
+function changeBodyToDark(){
     var elementBody = document.body;
     elementBody.classList.toggle("bg-color-dark-body");
 }
 
-function changeMenu(){
+function changeMenuToDark(){
     var elementsMenu = document.getElementsByClassName("bg-color-light");
     for(var i = 0; i < elementsMenu.length; i++) {
-        elementsMenu[i].classList.toggle("bg-color-light");
-        elementsMenu[i].classList.toggle("bg-color-dark");
+        toggleTheme(elementsMenu[i], "bg-color-dark", "bg-color-light"));
     }
 }
 
 function changeButtons(){
     var elementsMenu = document.getElementsByClassName("mbtn-light");
     for(var i = 0; i < elementsMenu.length; i++) {
-        elementsMenu[i].classList.toggle("mbtn-light");
-        elementsMenu[i].classList.toggle("mbtn-dark");
+        toggleTheme(elementsMenu[i], "mbtn-light", "mbtn-dark");
     }
 }
+
+function toggleTheme(element, classNameDark, classNameLight) {
+    element.classList.toggle(classNameDark);
+    element.classList.toggle(classNameLight);
+}
+
 
 
 
